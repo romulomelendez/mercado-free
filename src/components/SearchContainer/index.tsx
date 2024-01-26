ce5d409d3321950cb9f3c081071f65d99f01177f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-26 10:08:59 -0300

    Feat: input is responsive

--- src/components/SearchContainer/index.tsx
+++ src/components/SearchContainer/index.tsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci"
 import { Searcher } from "../"
 
 export const SearchContainer: React.FC = () => (
-    <div className="flex gap-2 shadow-md justify-center items-center bg-white rounded-sm p-1">
+    <div className="flex gap-2 w-full shadow-md justify-center items-center bg-white rounded-sm p-1">
         <CiSearch color="#cecece" size={20} />
         <Searcher />
     </div>
--- src/components/Searcher/index.tsx
+++ src/components/Searcher/index.tsx
@@ -1,7 +1,7 @@
 export const Searcher: React.FC = () => (
     <input
         type="text"
-        placeholder="I'm searching for..."
-        className="p-2 h-8 text-lg text-semibold outline-none text-gray-600 rounded-sm placeholder:text-gray-300"
+        placeholder="I'm looking for..."
+        className="p-2 h-8 w-full text-lg text-semibold outline-none text-gray-600 rounded-sm placeholder:text-gray-300"
     />
 )
