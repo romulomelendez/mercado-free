98d6e6896234cd3936e890bbdf331f8276327ea7
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-02-01 04:04:21 -0300

    link to home added in app logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom"
 import MercadoFreePageLogo from "../../assets/mercado-free-page-logo.png"
-// import MercadoFreeLogo from "../../assets/mercado-free-logo.png"
 
-export const Logo: React.FC = () => <img src={MercadoFreePageLogo} className="cursor-pointer" alt="Logo" height="45" width="45" />
+export const Logo: React.FC = () => (
+    <Link to="/">
+        <img src={MercadoFreePageLogo} className="cursor-pointer" alt="Logo" height="45" width="45" />
+    </Link>
+)
